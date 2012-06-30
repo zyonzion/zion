f734df8a85eafc0d35131a0766555f91f811ff7b
zyonzion <zyonzion@gmail.com>
2012-06-29 18:07:13 -1000

    Base rails project and mySQL yml

---
+++ zionApp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZionApp::Application.config.secret_token = '8337ec333dae480747d466083e00bcec62ab21d5e7fb73ff38d25744b85e0592936c05d72df6f5dd5983cfc1eadb29a4fa4b74eb2528bc5b9d330d50f61ce782'
